b1faed6b415b8369a0d0786e188f32e53465e3e8
Tyan_Pavel <ptyan_1@edu.hse.ru>
2023-11-06 00:03:34 +0300

    final

--- next.config.js
+++ next.config.js
@@ -1,21 +1,23 @@
+/* eslint-disable no-undef */
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+    reactStrictMode: true,
+};
 
 //module.exports = nextConfig;
 
 module.exports = {
-  reactStrictMode: true,
-  images: {
-    domains: ['courses-top.ru']
-  },
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/i,
-      use: ['@svgr/webpack'],
-    });
+    reactStrictMode: true,
+    images: {
+        domains: ['courses-top.ru'],
+    },
+    webpack(config) {
+        config.module.rules.push({
+            test: /\.svg$/i,
+            use: ['@svgr/webpack'],
+        });
 
-    return config;
-  }
-}
+        return config;
+    },
+};
